c6554eddb99d8d1bb02e5573177caeec0eafe1c9
tubone24 <tubo.yyyuuu@gmail.com>
2020-03-01 18:46:56 +0900

    first release

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # redmine_slack_notify
+
+![logo](./docs/images/logo.png)
+
+[![license](https://img.shields.io/github/license/tubone24/elixir_performance_tool.svg)](LICENSE)
+[![standard-readme compliant](https://img.shields.io/badge/readme%20style-standard-brightgreen.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+The script that access the Redmine server in the private environment, detects ticket updates, and notifies Slack.
+
+## Table of Contents
+
+- [Background](#background)
+- [Install](#install)
+- [Usage](#usage)
+- [API](#api)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Background
+
+
+In the workplace where I work, I use Redmine built in a private environment for sharing information between stakeholders.
+
+I wanted to share the update of Redmine Issue and its contents to Slack used by the development team,
+
+Since Redmine server can only be accessed in a private environment, Slack's RSS Integration could not be used.
+
+So, I thought about creating a Python script that runs on the PC (Raspberry PI) that was left behind in development, and notified Redmine of updates via the script.
+
+## Install
+
+### Precondition
+
+- Python3.6 or more
+
+### Install dependencies
+
+```
+pip install -r requirements.txt
+```
+
+### Edit your environment
+
+```
+mv .env.sample .env
+vi .env
+```
+
+Config(.env)
+
+```
+URL=https:/xxxxxxx/xxxxxx/issues.csv                             # Endpoint that download issues.csv on your redmine server(!Check filter and must sort by updated DESC)
+WEB_HOOK_URL_DAILY=https://hooks.slack.com/services/xxxxxx       # Incoming WEBHOOK for Daily notify
+WEB_HOOK_URL_EACH=https://hooks.slack.com/services/xxxx          # Incoming WEBHOOK notify when detecting updating 
+LOOP_INTERVAL=1       # Loop interval for detecting redmines updating
+DAILY_HOUR=9          # Daily notify hours
+DAILY_MINUTES=10      # Daily notify minutes (this examples sets 9:10)
+```
+
+## Usage
+
+```
+cd src 
+python main.py
+```
+
+## Contributing
+
+See [the contributing file](CONTRIBUTING.md)!
+
+PRs accepted.
+
+Small note: If editing the Readme, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
+
+## License
+
+[MIT © tubone24.](LICENSE)
---
+++ src/main.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""The script that access the Redmine server in the private environment, detects ticket updates, and notifies Slack.
+
+     * Access the Redmine Server and download issues.csv
+     * Parse issues.csv and check any updates
+     * When updates, extract contents and push notify to Slack incoming webhook
+     * Set the daily time on your config, aggregate all issues and notify all issues by 3days before
+
+Todo:
+    * Any Tests
+    * Documents
+    * Install manual for Raspberry PI
+
+"""
+
+import json
+import csv
+import textwrap
+from datetime import datetime, timedelta, timezone
+from time import sleep
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import requests
+from retrying import retry
+
+dotenv_path = join(dirname(__file__), "../.env")
+load_dotenv(dotenv_path)
+
+JST = timezone(timedelta(hours=+9))
+
+SINCEDB_PATH = join(dirname(__file__), "../since_db.txt")
+ISSUE_CSV_PATH = join(dirname(__file__), "../issues.csv")
+URL = os.environ.get("URL")
+WEB_HOOK_URL_DAILY = os.environ.get("WEB_HOOK_URL_DAILY")
+WEB_HOOK_URL_EACH = os.environ.get("WEB_HOOK_URL_EACH")
+LOOP_INTERVAL = int(os.environ.get("LOOP_INTERVAL"))
+DAILY_HOUR = int(os.environ.get("DAILY_HOUR"))
+DAILY_MINUTES = int(os.environ.get("DAILY_MINUTES"))
+
+
+def create_summary_text(row):
+    """Creating summary text by latest notes or description.
+
+    If there is the latest note in the Redmine ticket, get the note, otherwise, get the description.
+
+    Args:
+        row(OrderDict): Redmine's ticket row
+
+    Returns:
+        str: formatted summary text
+    """
+    content_text = ""
+    if row["最新の注記"] != "":
+        content_text = "【更新】" + row["最新の注記"]
+    elif row["説明"] != "":
+        content_text = "【新規】" + row["説明"]
+    return wrap_long_text(sanitize_contents_text(content_text))
+
+
+def wrap_long_text(text):
+    """Wrap long text for slack display
+
+    Args:
+        text(text): Long Text
+
+    Returns:
+        str: wrapped text
+
+    """
+    text_list = textwrap.wrap(text, 40)
+    return "\n　　　　　　".join(text_list)
+
+
+@retry(stop_max_attempt_number=3, wait_incrementing_start=1000, wait_incrementing_increment=1000)
+def notify_slack_daily(rows, now):
+    text_prefix = "デイリーRedmine更新チケットまとめ\n\n"
+    text_suffix = "\n\n\n`更新時間:{now_time}`".format(now_time=now.strftime("%Y-%m-%dT%H:%M:%S.%f%z"))
+    text_list = []
+    for row in rows:
+        content_text = sanitize_contents_text(create_summary_text(row))
+        bleak_str = "=" * 60
+        text = bleak_str + "\n\n番号　　　： `{id}` \n題名　　　： *{title}* \n" \
+                           "ステータス：{status}\n担当者　　：{person}\n更新日　　：{updated}\n" \
+                           "概要/状況 ：{content}\n\n".format(id=row["#"],
+                                                         title=row["題名"],
+                                                         status=row["ステータス"],
+                                                         person=row["担当者"],
+                                                         updated=row["更新日"],
+                                                         content=content_text)
+        text_list.append(text)
+    payload = {"text": text_prefix + "\n".join(text_list) + text_suffix}
+    requests.post(WEB_HOOK_URL_DAILY, json.dumps(payload), timeout=(3.0, 7.5))
+
+
+@retry(stop_max_attempt_number=3, wait_incrementing_start=1000, wait_incrementing_increment=1000)
+def notify_slack_each(rows):
+    bleak_str = "=" * 60
+    text_prefix = "REDMINEの更新を検知\n\n"
+    content_text = create_summary_text(rows[0])
+    text = bleak_str + "\n\n番号　　　： `{id}` \n題名　　　： *{title}* \n" \
+                       "ステータス：{status}\n担当者　　：{person}\n更新日　　：{updated}\n" \
+                       "概要/状況 ：{content}\n\n".format(id=rows[0]["#"],
+                                                     title=rows[0]["題名"],
+                                                     status=rows[0]["ステータス"],
+                                                     person=rows[0]["担当者"],
+                                                     updated=rows[0]["更新日"],
+                                                     content=content_text)
+    payload = {"text": text_prefix + text}
+    requests.post(WEB_HOOK_URL_EACH, json.dumps(payload), timeout=(3.0, 7.5))
+
+
+def notify_slack_error(error):
+    try:
+        print(error)
+        payload = {"text": "*ERROR OCCURRED!!*" + "```" + str(error) + "```"}
+        requests.post(WEB_HOOK_URL_EACH, json.dumps(payload), timeout=(3.0, 7.5))
+    except Exception:
+        pass
+
+
+def before_3days_msg(rows, now):
+    return_msg = []
+    for row in rows:
+        print(row)
+        updated = datetime.strptime(row["更新日"], '%Y/%m/%d %H:%M').astimezone(JST)
+        if updated > now.astimezone(JST) - timedelta(days=3):
+            return_msg.append(row)
+    return return_msg
+
+
+def sanitize_contents_text(text):
+    format_text = text.replace("```", "").replace("\n", "").replace(" ", "").replace("`", "")
+    return format_text
+
+
+# def extract_entries(entries):
+#     notifies = []
+#     for entry in entries:
+#         title = entry.title.value
+#         p1 = re.compile(r"^.+(.+):")
+#         format_title = p1.sub("", title)
+#         p2 = re.compile(r"(.+):")
+#         status = p2.search(title).group()
+#         id = entry.id_
+#         updated = entry.updated
+#         updated_jst = updated.astimezone(JST)
+#         author_name = entry.authors[0].name
+#         content = entry.content.value
+#         p = re.compile(r"<[^>]*?>")
+#         text = p.sub("", content)
+#         format_text = sanitize_contents_text(text)
+#         # print(format_text)
+#         notifies.append({"title": format_title, "id": id, "updated": updated_jst,
+#         "author_name": author_name, "text": format_text, "status": status})
+#     return notifies
+
+
+@retry(stop_max_attempt_number=3, wait_incrementing_start=1000, wait_incrementing_increment=1000)
+def download_issues_csv():
+    response = requests.get(URL, timeout=(3.0, 7.5))
+    with open(ISSUE_CSV_PATH, 'wb') as saveFile:
+        saveFile.write(response.content)
+
+
+def load_issues_csv():
+    with open(ISSUE_CSV_PATH, "r", encoding="utf-8_sig") as f:
+        rows = []
+        reader = csv.DictReader(f)
+        for row in reader:
+            rows.append(row)
+        return rows
+
+
+def latest_update(rows):
+    tdatetime = datetime.strptime(rows[0]["更新日"], '%Y/%m/%d %H:%M')
+    return tdatetime
+
+
+def is_need_update(updated):
+    with open(SINCEDB_PATH, "r") as f:
+        last_updated = f.read()
+        print("last_updated: {last_updated}".format(last_updated=updated))
+        if last_updated == updated:
+            return False
+        return True
+
+
+def update_sincedb(updated):
+    with open(SINCEDB_PATH, "w") as f:
+        f.write(updated)
+
+
+def check_daily_time(now):
+    if now.hour == DAILY_HOUR and DAILY_MINUTES < now.minute <= DAILY_MINUTES + LOOP_INTERVAL:
+        return True
+    return False
+
+
+def main():
+    now = datetime.now()
+    download_issues_csv()
+    rows = load_issues_csv()
+
+    now_jst = now.astimezone(JST)
+    updated_jst = latest_update(rows)
+    updated_jst = updated_jst.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+    if is_need_update(updated_jst):
+        notify_slack_each(rows)
+        update_sincedb(updated_jst)
+    else:
+        print("no update! skip")
+
+    if check_daily_time(now_jst):
+        print("daily")
+        daily_msg = before_3days_msg(rows, now)
+        print(daily_msg)
+        notify_slack_daily(daily_msg, now)
+
+
+def loop_main():
+    while True:
+        try:
+            main()
+        except Exception as e:
+            notify_slack_error(e)
+        sleep(LOOP_INTERVAL * 60)
+
+
+if __name__ == "__main__":
+    loop_main()
